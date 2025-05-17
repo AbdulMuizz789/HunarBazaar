5d35efaa590ea5881f82e04f385d6c787d69d31f
AbdulMuizz789 <muizzabdul789@gmail.com>
2025-05-17 15:47:23 +0400

    Added gig and user data models

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const http = require('http');
@@ -8,6 +9,10 @@ const app = express();
 app.use(cors());
 const server = http.createServer(app);
 app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(()=>console.log('Connected to MongoDB'))
+    .catch(err=>console.error('MongoDB connction error:',err));
 
 app.use((req, res) => {
     res.status(404).json({error: 'Route not found'});
